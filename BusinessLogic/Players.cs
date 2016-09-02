8152d5333e540344704a903fb7223db733f8e786
Safin Ruslan <ruslan.safin@byndyusoft.com>
2016-09-02 10:46:40 +0500

    вынес Players в отдельный файл

---
+++ BusinessLogic/Players.cs
@@ -0,0 +1,13 @@
+namespace BusinessLogic
+{
+    public static class Players
+    {
+        public static string Ruslan = "Ruslan";
+        public static string Anatron = "Anatron";
+        public static string Semen = "Semen";
+        public static string Gleb = "Gleb";
+        public static string Igor = "Igor";
+        public static string Levch = "Levch";
+        public static string Serega = "Serega";
+    }
+}
--- BusinessLogic/Store.cs
+++ BusinessLogic/Store.cs
@@ -137,15 +137,4 @@ namespace BusinessLogic
         Seven,
         Score
     }
-    
-    public static class Players
-    {
-        public static string Ruslan = "Ruslan";
-        public static string Anatron = "Anatron";
-        public static string Semen = "Semen";
-        public static string Gleb = "Gleb";
-        public static string Igor = "Igor";
-        public static string Levch = "Levch";
-        public static string Serega = "Serega";
-    }
 }
